d26dab5eba5166dbd1c57669c9b3b367e5311d03
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-08 21:54:17 +0000

    udacity loops practice

---
+++ udacity-loops.js
@@ -0,0 +1,7 @@
+//Count to 10000
+
+let x = 1; // start at 1
+while (x <= 10000) {  // check if count is still under 10000
+  console.log(x + " mississippi");  //print count
+  x = x + 1;    //increment
+}
