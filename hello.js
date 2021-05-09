76547e2a7c9b247c5e8e15eec4b84dcc8c616c14
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-09 15:17:14 +0000

    updated hello.js and added file isEven function to check if a number is even

--- hello.js
+++ hello.js
@@ -1,7 +1,19 @@
-const sayHello = function (name) {
+/* const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello("Van");
 sayHello("Caliban");
-sayHello("Ferdinand");
+sayHello("Ferdinand"); */
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello("John");
+console.log(greeting);
---
+++ isEven.js
@@ -0,0 +1,7 @@
+/*Function to check if a number is even */
+const isEven = function (num) {
+  return num % 2  === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
