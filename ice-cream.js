441aaa233cbd6a18577b1732c783c4f1c54a7c1d
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-09 19:18:34 +0000

    Assignment ice cream flavors Arrays

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+//Add "root beer" flavor to the end of array and log it 
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//log the first element of the array
+console.log(iceCreamFlavours[0]);
+
+//log the last element of the array
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//log the total number of flavors
+console.log(iceCreamFlavours.length);
